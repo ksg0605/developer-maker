10f07e136ea1db88f471bf85da72fdcaa0cd07bf
ksg0605 <ksg0605@gmail.com>
2022-02-19 16:44:38 +0900

    add entity

---
+++ src/main/java/ac/kr/jejunu/developermaker/entity/Developer.java
@@ -0,0 +1,53 @@
+package ac.kr.jejunu.developermaker.entity;
+
+import ac.kr.jejunu.developermaker.type.DeveloperLevel;
+import ac.kr.jejunu.developermaker.type.DeveloperSkillType;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * packageName            : ac.kr.jejunu.developermaker.entity
+ * fileName              : Developer
+ * author                : sunkyu
+ * date                  : 2022/02/19
+ * description           :
+ * ===========================================================
+ * DATE              AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2022/02/19           sunkyu             최초 생성
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperSkillType developerSkillType;
+
+    private Integer experienceYears;
+    private String memberId;
+    private String name;
+    private Integer age;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
