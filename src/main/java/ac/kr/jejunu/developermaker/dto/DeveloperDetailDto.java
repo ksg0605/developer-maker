90a5f9502f90e41ecca25f444ef967b634e4a1b4
ksg0605 <ksg0605@gmail.com>
2022-02-19 22:13:24 +0900

    add DeveloperDetailDto

---
+++ src/main/java/ac/kr/jejunu/developermaker/dto/DeveloperDetailDto.java
@@ -0,0 +1,43 @@
+package ac.kr.jejunu.developermaker.dto;
+
+import ac.kr.jejunu.developermaker.entity.Developer;
+import ac.kr.jejunu.developermaker.type.DeveloperLevel;
+import ac.kr.jejunu.developermaker.type.DeveloperSkillType;
+import lombok.*;
+
+/**
+ * packageName            : ac.kr.jejunu.developermaker.dto
+ * fileName              : DeveloperDetailDto
+ * author                : sunkyu
+ * date                  : 2022/02/19
+ * description           :
+ * ===========================================================
+ * DATE              AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2022/02/19           sunkyu             최초 생성
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeveloperDetailDto {
+    private DeveloperLevel developerLevel;
+    private DeveloperSkillType developerSkillType;
+    private Integer experienceYears;
+    private String memberId;
+    private String name;
+    private Integer age;
+
+    public static DeveloperDetailDto fromEntity(Developer developer) {
+        return DeveloperDetailDto.builder()
+                .developerLevel(developer.getDeveloperLevel())
+                .developerSkillType(developer.getDeveloperSkillType())
+                .experienceYears(developer.getExperienceYears())
+                .memberId(developer.getMemberId())
+                .name(developer.getName())
+                .age(developer.getAge())
+                .build();
+    }
+}
