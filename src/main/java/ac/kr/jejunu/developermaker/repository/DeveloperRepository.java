8400a652fbfa7927b9bc926ee095a49b22b31ce5
ksg0605 <ksg0605@gmail.com>
2022-02-19 16:44:56 +0900

    add repository

---
+++ src/main/java/ac/kr/jejunu/developermaker/repository/DeveloperRepository.java
@@ -0,0 +1,21 @@
+package ac.kr.jejunu.developermaker.repository;
+
+import ac.kr.jejunu.developermaker.entity.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * packageName            : ac.kr.jejunu.developermaker.repository
+ * fileName              : DeveloperRepository
+ * author                : sunkyu
+ * date                  : 2022/02/19
+ * description           :
+ * ===========================================================
+ * DATE              AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2022/02/19           sunkyu             최초 생성
+ */
+@Repository
+public interface DeveloperRepository
+        extends JpaRepository<Developer, Long> {
+}
