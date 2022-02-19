e6d0495cfb71cec967bf838728ec550982e3773d
ksg0605 <ksg0605@gmail.com>
2022-02-19 20:51:15 +0900

    edit Service

--- src/main/java/ac/kr/jejunu/developermaker/service/DMakerService.java
+++ src/main/java/ac/kr/jejunu/developermaker/service/DMakerService.java
@@ -1,13 +1,18 @@
 package ac.kr.jejunu.developermaker.service;
 
+import ac.kr.jejunu.developermaker.dto.CreateDeveloper;
 import ac.kr.jejunu.developermaker.entity.Developer;
+import ac.kr.jejunu.developermaker.exception.DMakerException;
 import ac.kr.jejunu.developermaker.repository.DeveloperRepository;
 import ac.kr.jejunu.developermaker.type.DeveloperLevel;
-import ac.kr.jejunu.developermaker.type.DeveloperSkillType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import static ac.kr.jejunu.developermaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static ac.kr.jejunu.developermaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
 
 /**
  * packageName            : ac.kr.jejunu.developermaker.service
@@ -26,15 +31,37 @@ public class DMakerService {
     private final DeveloperRepository developerRepository;
 
     @Transactional
-    public void createDeveloper() {
+    public CreateDeveloper.Response createDeveloper(CreateDeveloper.@Valid Request request) {
+        validateCreateDeveloperRequest(request);
+
         Developer developer = Developer.builder()
-                .developerLevel(DeveloperLevel.JUNIOR)
-                .developerSkillType(DeveloperSkillType.FRONT_END)
-                .experienceYears(2)
-                .name("Olaf")
-                .age(5)
+                .developerLevel(request.getDeveloperLevel())
+                .developerSkillType(request.getDeveloperSkillType())
+                .experienceYears(request.getExperienceYears())
+                .memberId(request.getMemberId())
+                .name(request.getName())
+                .age(request.getAge())
                 .build();
 
         developerRepository.save(developer);
+        return CreateDeveloper.Response.fromEntity(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+
+        developerRepository.findByMemberId(request.getMemberId())
+                .ifPresent((developer -> {
+                    throw new DMakerException(DUPLICATED_MEMBER_ID);
+                }));
+        if (developerLevel == DeveloperLevel.SENIOR && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUGNIOR && experienceYears < 4 || experienceYears > 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
     }
 }
