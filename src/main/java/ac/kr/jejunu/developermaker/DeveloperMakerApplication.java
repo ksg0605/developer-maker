9347779607d6a67a6292ab29cfdef22bda3d6d32
ksg0605 <ksg0605@gmail.com>
2022-02-19 15:09:33 +0900

    Initial commit

---
+++ src/main/java/ac/kr/jejunu/developermaker/DeveloperMakerApplication.java
@@ -0,0 +1,13 @@
+package ac.kr.jejunu.developermaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/kr/jejunu/developermaker/DeveloperMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.jejunu.developermaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
