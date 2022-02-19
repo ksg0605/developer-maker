b85a9cc310826c42902676aee26fdb6105a4ebb1
ksg0605 <ksg0605@gmail.com>
2022-02-19 20:51:00 +0900

    add Controller

--- src/main/java/ac/kr/jejunu/developermaker/controller/DMakerController.java
+++ src/main/java/ac/kr/jejunu/developermaker/controller/DMakerController.java
@@ -1,13 +1,16 @@
 package ac.kr.jejunu.developermaker.controller;
 
+import ac.kr.jejunu.developermaker.dto.CreateDeveloper;
 import ac.kr.jejunu.developermaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -36,13 +39,10 @@ public class DMakerController {
         return Arrays.asList("snow", "Elsa", "Olaf");
     }
 
-    @GetMapping("/create-developer")
-    public List<String> createDevelopers() {
-        //GET /developers HTTP/1.1
-        log.info("GET /create-developer HTTP/1.1");
+    @PostMapping("/create-developer")
+    public CreateDeveloper.Response createDevelopers(@Valid @RequestBody CreateDeveloper.Request request) {
+        log.info("request : {}", request);
 
-        dMakerService.createDeveloper();
-
-        return Collections.singletonList("Olaf");
+        return dMakerService.createDeveloper(request);
     }
 }
